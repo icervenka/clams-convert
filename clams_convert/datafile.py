4707e4bf500fa05da525166ddcc9a51c86d8a429
Igor Cervenka <wormikk@gmail.com>
2021-09-16 14:40:39 +0200

    Implemented initialization of data descriptors

--- clams_convert/datafile.py
+++ clams_convert/datafile.py
@@ -41,3 +41,37 @@ class Datafile:
                 "only path and DataFrame are accepted.")
         self.__initialize()
         return self
+
+    def init_start_end_date(self, round_mins=True):
+        self.start_date = sorted(self.data.date_time)[0]
+        self.end_date = sorted(self.data.date_time)[-1]
+        if round_mins:
+            self.start_date = round_minutes(self.start_date)
+            self.end_date = round_minutes(self.end_date)
+
+    def init_subjects(self, rename_subject_mapping):
+        if rename_subject_mapping is not None:
+            self.rename_subjects(rename_subject_mapping)
+        self.subjects = list(set(self.data.subject.tolist()))
+        for s in self.subjects:
+            self.subject_split_data[s] = self.data.query('subject == @s')
+
+    def init_num_observations(self):
+        try:
+            self.num_observations = {k: s.shape[0] for (k, s) in self.subject_split_data.items()}
+        except ValueError:
+            print("Subject data not initialized.")
+
+    def init_parameters(self):
+        self.parameters = self.get_parameters()
+
+    def init_light_column(self):
+        if self.dark_start is None or self.dark_end is None:
+            raise ValueError("Light column is not present in the data " +
+                "and dark_start or dark_end attributes are not set.")
+        else:
+            ts = self.data['date_time'].time()
+            ds = str_to_time(self.dark_start)
+            de = str_to_time(self.dark_end)
+            # TODO this will not work
+            self.data['light'] = pd.Series(np.where(ts >= ds and ts < de, 0, 1))
